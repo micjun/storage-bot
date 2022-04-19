317263807cd0d005874a8c2a5acd120af9935724
micjun <jung.michael@protonmail.com>
2022-04-19 19:42:54 -0400

    refactor: code clean up

--- server/server.js
+++ server/server.js
@@ -1,25 +1,19 @@
 const { App } = require('@slack/bolt');
 const db = require('./models/conversationModel');
+
 require('dotenv').config();
 // Initializes your app with your bot token and signing secret
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  // socketMode: true, // enable the following to use socket mode
+  // socketMode: true, // alternative socket mode
   // appToken: process.env.APP_TOKEN,
 });
+
 const store = [];
 const conversation = [];
 const textArr = [];
-// app.command('/knowledge', async ({ command, ack, say }) => {
-//   try {
-//     await ack();
-//     say('Conversation stored');
-//   } catch (error) {
-//     console.log('err');
-//     console.error(error);
-//   }
-// });
 
 app.message('store conversation id', async ({ message, say }) => {
   try {
