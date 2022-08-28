52a932aa7492113ecda7ea9e9b06db9c60a204ad
micjun <jung.michael@protonmail.com>
2022-08-28 13:41:08 -0400

    socket mode

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  // socketMode: true, // alternative socket mode
-  // appToken: process.env.APP_TOKEN,
+  socketMode: true, // alternative socket mode
+  appToken: process.env.APP_TOKEN,
 });
 
 const channellist = {};
@@ -217,5 +217,5 @@ app.message('user id', async ({ message, say }) => {
 
 (async () => {
   await app.start(process.env.PORT || 3000);
-  console.log('⚡️ Bolt app is running!');
+  console.log('app is running!');
 })();
